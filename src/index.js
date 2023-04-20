7c7efeda48fe66289d52c927d5bf896c4a5a9664
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-04-20 12:17:52 +0200

    Init project and add global styles

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './styles/normalize.scss';
+import './styles/global.scss';
+
+
+
+
+
+const container = document.querySelector('#root')
+const root = createRoot(container)
+
+root.render(
+  
+      <App tab="home"/>
+  
+);
+
+
