77155821c21a01afce50eb0718f816ea5ce71318
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-04-20 12:30:49 +0200

    Add Container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-
+import Container from "./components/Container";
 
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <main>
+       <Container>
+
+      </Container>
+    </main>
+  
   );
 }
 
