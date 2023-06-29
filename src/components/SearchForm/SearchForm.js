678809a8ec9bf2bfdf553f199feaa8abfee1a8fa
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-06-29 14:30:11 +0200

    Add filteredPurposes

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -18,6 +18,7 @@ const SearchForm = () => {
   const [filteredCities, setFilteredCities] = useState([]);
   const [filteredPropertyTypes, setFilteredPropertyTypes] = useState([]);
   const [filteredDistricts, setFilteredDistricts] = useState([]);
+  const [filteredPurposes, setFilteredPurposes] = useState([]);
   const [areaMin, setAreaMin] = useState('');
   const [areaMax, setAreaMax] = useState('');
 
@@ -42,45 +43,55 @@ const SearchForm = () => {
 
   useEffect(() => {
     const cities = []
-    for (let propertyData of properties) {
-      if (property === propertyData.type) {
-        cities.push(propertyData.city);
-      }
-    }
-    if (property) {
-      setFilteredCities([...new Set(cities)]);
-    } else {
-      setFilteredCities(uniquePropertyCities);
-    }
-  }, [property]);
-
-  useEffect(() => {
     const propertyTypes = [];
     const districts = [];
+    const purposes =[];
 
     for (let propertyData of properties) {
       if (city === propertyData.city) {
         propertyTypes.push(propertyData.type);
-        districts.push(propertyData.district)
+        districts.push(propertyData.district);
+      }
+      if (property === propertyData.type) {
+        cities.push(propertyData.city);
+        purposes.push(propertyData.purpose);
+      }
+      if(purpose === propertyData.purpose) {
+        cities.push(propertyData.city);
+        propertyTypes.push(propertyData.type);
       }
     }
+    console.log('cities', cities);
+    console.log('propertyTypes', propertyTypes);
+    console.log('districts', districts);
+    console.log('purposes', purposes);
     if (city) {
       setFilteredPropertyTypes([...new Set(propertyTypes)]);
       setFilteredDistricts([...new Set(districts)]);
       setIsDistrictDisabled(false);
     }
+    if (property) {
+      setFilteredCities([...new Set(cities)]);
+      setFilteredPurposes([...new Set(purposes)]);
+    }
+    if (purpose) {
+      setFilteredPropertyTypes([...new Set(propertyTypes)]);
+      setFilteredCities([...new Set(cities)]);
+    }
     else {
       setFilteredPropertyTypes(uniquePropertyTypes);
       setFilteredDistricts(uniquePropertyDistricts);
+      setFilteredCities(uniquePropertyCities);
+      setFilteredPurposes(uniquePropertyPurposes);
     }
-  }, [city]);
+  }, [city, property, purpose]);
 
 
   return (
     <div>
       <form className={styles.form} onSubmit={handleSubmit}>
         <Select options={filteredPropertyTypes} label="Rodzaj nieruchomości" name='Rodzaj nieruchomości' value={property} onChange={(e) => setProperty(e.target.value)} />
-        <Select options={uniquePropertyPurposes} label="Typ transakcji" name='Typ transakcji' value={purpose} onChange={(e) => setPurpose(e.target.value)} />
+        <Select options={filteredPurposes} label="Typ transakcji" name='Typ transakcji' value={purpose} onChange={(e) => setPurpose(e.target.value)} />
         <Select options={filteredCities} label="Miasto" name="Miasto" value={city} onChange={(e) => setCity(e.target.value)} />
         <Select options={filteredDistricts} label="Dzielnica" name="Dzielnica" value={district} disabled={isDistrictDisabled} onChange={(e) => setDistrict(e.target.value)} />
         <Select options={areas} name="PowierzchniaMin" label="Powierzchnia min." value={areaMin} onChange={(e) => setAreaMin(e.target.value)}></Select>
